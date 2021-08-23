7b83af58c008912e1ac59bf09d8cbce231678043
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-08-23 12:56:37 +0200

    make fave pagination

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Participant;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use App\Helpers\APIHelpers;
@@ -76,30 +77,51 @@ class FavoriteController extends Controller
             $response = APIHelpers::createApiResponse(true , 406 ,  'تم حظر حسابك', 'تم حظر حسابك' , null, $request->lang );
             return response()->json($response , 406);
         }else {
-            $favorites = Favorite::select('id','product_id','user_id')
+            $products = Favorite::select('id','product_id','user_id')
                                  ->with('Product')
                                  ->where('user_id', $user->id)
                                  ->orderBy('id','desc')
-                                 ->get();
+                ->simplePaginate(12);
 
-            $inc = 0;
-            $data = null;
-            foreach ($favorites as $key => $row){
-                $product = Product::where('id',$row->product_id)->first();
-                if($product != null){
-                    if($product->status == 1 && $product->deleted == 0 && $product->publish == 'Y'){
-                        $data[$inc]['id'] = $product->id;
-                        $data[$inc]['title'] = $product->title;
-                        $data[$inc]['image'] = $product->main_image;
-                        $data[$inc]['price'] = $product->price;
-                        $data[$inc]['description'] = $product->description;
-                        $inc =$inc + 1;
+//            $inc = 0;
+//            $data = null;
+//            foreach ($favorites as $key => $row){
+//                $product = Product::where('id',$row->product_id)->first();
+//                if($product != null){
+//                    if($product->status == 1 && $product->deleted == 0 && $product->publish == 'Y'){
+//                        $data[$inc]['id'] = $product->id;
+//                        $data[$inc]['title'] = $product->title;
+//                        $data[$inc]['image'] = $product->main_image;
+//                        $data[$inc]['price'] = $product->price;
+//                        $data[$inc]['description'] = $product->description;
+//                        $inc =$inc + 1;
+//                    }
+//                }
+//            }
+            for ($i = 0; $i < count($products); $i++) {
+                $products[$i]['Product']->price  = number_format((float)(  $products[$i]['Product']->price ), 3);
+                if ($user) {
+                    $favorite = Favorite::where('user_id', $user->id)->where('product_id', $products[$i]['product_id'])->first();
+                    if ($favorite) {
+                        $products[$i]['Product']->favorite  = true;
+                    } else {
+                        $products[$i]['Product']->favorite = false;
                     }
+
+                    $conversation = Participant::where('ad_product_id', $products[$i]['product_id'])->where('user_id', $user->id)->first();
+                    if ($conversation == null) {
+                        $products[$i]['Product']->conversation_id = 0;
+                    } else {
+                        $products[$i]['Product']->conversation_id = $conversation->conversation_id;
+                    }
+                } else {
+                    $products[$i]['favorite'] = false;
+                    $products[$i]['conversation_id'] = 0;
                 }
             }
 
-            if(count($favorites) > 0) {
-                $response = APIHelpers::createApiResponse(false, 200, '', '', $data, $request->lang);
+            if(count($products) > 0) {
+                $response = APIHelpers::createApiResponse(false, 200, '', '', $products, $request->lang);
             }else{
                 $response = APIHelpers::createApiResponse(false, 200, 'no item favorite to show', 'لا يوجد عناصر للعرض', null, $request->lang);
             }
