ea12499f0bcf6bf2c4a416cf8089c7e0d7f8ab72
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-12-06 12:09:27 +0200

    sd

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1678,6 +1678,7 @@ class ProductController extends Controller
         }
         $validator = Validator::make($request->all(), [
             'product_id' => 'required|exists:products,id',
+            'content' => 'required',
         ]);
         if ($validator->fails()) {
             $response = APIHelpers::createApiResponse(true, 406, $validator->errors()->first(), $validator->errors()->first(), null, $request->lang);
@@ -1691,6 +1692,7 @@ class ProductController extends Controller
             $favorite = new Product_report();
             $favorite->user_id = $user->id;
             $favorite->product_id = $request->product_id;
+            $favorite->content = $request->content;
             $favorite->save();
             $response = APIHelpers::createApiResponse(false, 200, '', '', $favorite, $request->lang);
             return response()->json($response, 200);
