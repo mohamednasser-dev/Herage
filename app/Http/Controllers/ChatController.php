4f93e0b3761b4fc847c45cb79e68c5f998f59332
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2022-01-10 11:28:12 +0200

    fix

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -196,7 +196,7 @@ class ChatController extends Controller
 
     public function get_ad_message(Request $request)
     {
-       
+
         $user_id = auth()->user()->id;
 //        $partic = Participant::where('user_id',$user_id)->where('ad_product_id',$request->id)->first();
 
@@ -226,7 +226,7 @@ class ChatController extends Controller
             $days = Message::where('ad_product_id', $request->id)
                 ->where('conversation_id', $request->conversation_id)
                 ->select('id', 'message', 'type', 'user_id', 'conversation_id', 'ad_product_id', 'created_at')
-                ->orderBy('created_at', 'desc')
+                ->orderBy('created_at', 'asc')
                 ->get()
                 ->map(function ($messages) use ($user_id) {
                     $messages->time = $messages->created_at->format('g:i a');
